392fb2ea834b61203dedbbda72bd96774d9d3367
hiroyuki <hiroyukitamura@gmail.com>
2019-04-27 05:28:28 +0900

    create initial phrase data in json

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const functions = require('firebase-functions');
-const {Sample} = require('./transpiled/index');
+const {FirestoreClient} = require('./transpiled/index');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send(new Sample().test());
+ response.send(new FirestoreClient().uploadInitialPhrases());
 });
---
+++ src/main/kotlin/FirestoreClient.kt
@@ -0,0 +1,7 @@
+class FirestoreClient {
+
+    fun uploadInitialPhrases(){
+        val json = require("../resources/phrase.json")
+        print(json)
+    }
+}
